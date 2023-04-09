09b7e614da7dadd99dbacf8c1cc2e77d0d4cd2b3
Xefise <xefise@gmail.com>
2023-04-09 20:38:19 +0300

    Refactor OwnershipForm. Fix render keys

--- src/components/ownership_forms/OnwershipForm.tsx
+++ src/components/ownership_forms/OnwershipForm.tsx
@@ -5,10 +5,9 @@ import OwnershipMainForm from "./OwnershipMainForm/OwnershipMainForm";
 import {FormProvider, SubmitHandler, useFieldArray, useForm} from "react-hook-form";
 import OwnershipFormModel from "../../models/OwnershipFormModel";
 import LabeledInput from "../LabeledInput/LabeledInput";
-import {maxLenghtEr, maxLenghtFileMes, minLenghtEr, requiredErMes} from "../../tools/hookFormErrors";
-import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import {mixed, number, object, ObjectSchema, string} from "yup";
+import {ownershipFormSchema} from "./OwnershipFormSchema";
 
 export interface OnwershipFormProps {
 }
@@ -19,50 +18,7 @@ function OnwershipForm({}: OnwershipFormProps) {
   const [banksNum, setBanksNum] = useState<number>(1);
 
 
-  const photoValidSchema = yup.mixed()
-    .required("Вы не загрузили фото!")
-    .test("fileExists", "Вы не загрузили изображение!", (value: any) => {
-      return !(!value || value.length == 0 || !value[0].size);
-    })
-    .test("fileSize", maxLenghtFileMes("1МБ"), (value : any) => {
-      if (!value || value.length == 0 || !value[0].size) return false;
-      return value[0].size <= 1048576;
-    })
-    .test("fileFormat", "Выберите файл pdf, png или jpeg", (value : any) => {
-      if (!value || value.length == 0 || !value[0].type) return false;
-      return /(pdf|png|jpeg)$/.test(value[0].type);
-    });
-
-  const ownershipBankDetailsSchema = yup.object().shape({
-    bic: yup.number().required(requiredErMes).min(9, minLenghtEr(9).message).max(9, maxLenghtEr(9)).typeError(requiredErMes),
-    bankBranchName: yup.string().required(requiredErMes),
-    checkingAccount: yup.string().required(requiredErMes).min(20, minLenghtEr(20).message).max(20, maxLenghtEr(20)).typeError(requiredErMes),
-    correspondentAccount: yup.string().required(requiredErMes).min(20, minLenghtEr(20).message).max(20, maxLenghtEr(20)).typeError(requiredErMes),
-  })
-
-  const schema = yup.object().shape({
-    activityType: yup.string().required(requiredErMes),
-    inn: yup.number().required(requiredErMes).test(
-      'lenght', 'ИНН должен содержать 10 или 12 цифр',
-      (val) => !!(val && (val.toString().length == 10 || val.toString().length == 12))
-    ).typeError(requiredErMes),
-    scanInn: photoValidSchema,
-    ogrnip: yup.number().required(requiredErMes).test(
-      "lenght", "В числе должно быть 15 цифр!",
-      (val) => (val ? val.toString().length == 15 : true)
-    ).typeError(requiredErMes),
-    scanOgrnip: photoValidSchema,
-    registrationDate: yup.date().required(requiredErMes).typeError(requiredErMes),
-    scanEgrip: photoValidSchema,
-    scanLeaseAgreement: photoValidSchema,
-    noAgreement: yup.boolean(),
-    name: yup.string().max(80, maxLenghtEr(80).message),
-    shortName: yup.string().max(30, maxLenghtEr(30).message),
-
-    ownershipBankDetailsList: yup.array().of(ownershipBankDetailsSchema),
-  })
-
-  const hookForm = useForm<OwnershipFormModel>({resolver: yupResolver(schema),});
+  const hookForm = useForm<OwnershipFormModel>({resolver: yupResolver(ownershipFormSchema)});
   const {
     control,
     register,
@@ -92,7 +48,7 @@ function OnwershipForm({}: OnwershipFormProps) {
         </div>
         <OwnershipMainForm type={activityTypeText} stepComplete={() => {setStep(3); appendBankForm();}}/>
         {fields.map((field, i) => (
-          <div className={gstyles.form_section}>
+          <div key={`bank_${i}`} className={gstyles.form_section}>
             <LabeledInput key={`bank_${i}_a`} label={"БИК*"} placeholder={"ххххххххх"} type="number"
               propRegister={register(`ownershipBankDetailsList.${i}.bic`)}
               error={errors.ownershipBankDetailsList?.[i]?.bic?.message}/>
---
+++ src/components/ownership_forms/OwnershipFormSchema.ts
@@ -0,0 +1,45 @@
+import * as yup from "yup";
+import {maxLenghtEr, maxLenghtFileMes, minLenghtEr, requiredErMes} from "../../tools/hookFormErrors";
+
+export const photoValidSchema = yup.mixed()
+  .required("Вы не загрузили фото!")
+  .test("fileExists", "Вы не загрузили изображение!", (value: any) => {
+    return !(!value || value.length == 0 || !value[0].size);
+  })
+  .test("fileSize", maxLenghtFileMes("1МБ"), (value : any) => {
+    if (!value || value.length == 0 || !value[0].size) return false;
+    return value[0].size <= 1048576;
+  })
+  .test("fileFormat", "Выберите файл pdf, png или jpeg", (value : any) => {
+    if (!value || value.length == 0 || !value[0].type) return false;
+    return /(pdf|png|jpeg)$/.test(value[0].type);
+  });
+
+export const ownershipBankDetailsSchema = yup.object().shape({
+  bic: yup.number().required(requiredErMes).min(9, minLenghtEr(9).message).max(9, maxLenghtEr(9)).typeError(requiredErMes),
+  bankBranchName: yup.string().required(requiredErMes),
+  checkingAccount: yup.string().required(requiredErMes).min(20, minLenghtEr(20).message).max(20, maxLenghtEr(20)).typeError(requiredErMes),
+  correspondentAccount: yup.string().required(requiredErMes).min(20, minLenghtEr(20).message).max(20, maxLenghtEr(20)).typeError(requiredErMes),
+})
+
+export const ownershipFormSchema = yup.object().shape({
+  activityType: yup.string().required(requiredErMes),
+  inn: yup.number().required(requiredErMes).test(
+    'lenght', 'ИНН должен содержать 10 или 12 цифр',
+    (val) => !!(val && (val.toString().length == 10 || val.toString().length == 12))
+  ).typeError(requiredErMes),
+  scanInn: photoValidSchema,
+  ogrnip: yup.number().required(requiredErMes).test(
+    "lenght", "В числе должно быть 15 цифр!",
+    (val) => (val ? val.toString().length == 15 : true)
+  ).typeError(requiredErMes),
+  scanOgrnip: photoValidSchema,
+  registrationDate: yup.date().required(requiredErMes).typeError(requiredErMes),
+  scanEgrip: photoValidSchema,
+  scanLeaseAgreement: photoValidSchema,
+  noAgreement: yup.boolean(),
+  name: yup.string().max(80, maxLenghtEr(80).message),
+  shortName: yup.string().max(30, maxLenghtEr(30).message),
+
+  ownershipBankDetailsList: yup.array().of(ownershipBankDetailsSchema),
+})
